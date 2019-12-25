71c5cdbfd95836aa20b379a00c0d27587c101e5d
Mark Dietzler <mark.e.dietzler@gmail.com>
2019-12-25 13:39:21 -0800

    upload bare bones sausa mobile to blank repo

---
+++ app/src/main/java/team/four/sausa/ui/tabpagerAdapter.java
@@ -0,0 +1,31 @@
+package team.four.sausa.ui;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class tabpagerAdapter extends FragmentStatePagerAdapter {
+
+    String[] tabArray = new String[]{"One","Two","Three","Four"};
+
+    public tabpagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabArray[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
